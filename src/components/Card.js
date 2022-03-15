1b571f79d423613f549ef59a6140152a4a12b46b
Tommaso Ferrari <tommasoball@gmail.com>
2022-03-15 01:26:41 +0100

    css fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,35 +2,34 @@ import React from "react";
 import './Card.css'
 
 
-function Card(props){
-const data = props.data;
+function Card(props) {
 
-
-let alliance = 'none';
-if(data.alliance==='OW')
-    alliance='Oneworld';
-else if(data.alliance==='ST')
-    alliance='Sky Team';
-else if(data.alliance==='SA')
-    alliance='Star Alliance';
+    const data = props.data;
+    let alliance = 'none';
+    if (data.alliance === 'OW')
+        alliance = 'Oneworld';
+    else if (data.alliance === 'ST')
+        alliance = 'Sky Team';
+    else if (data.alliance === 'SA')
+        alliance = 'Star Alliance';
 
     return (
         <div className="card-container">
-               <img src={"https://www.kayak.com" + data.logoURL} className='logo-airline' alt=''></img>
-               <div className="card-title">
-               {data.name}
-                    <div className="details">
-                        <div className="alliance">
-                            {alliance!=='none' && alliance}
-                        </div>
-                        <div className="phone">
-                            {data.phone}
-                        </div>
-                        <div className="site">
-                            {data.site}
-                        </div>
+            <img src={"https://www.kayak.com" + data.logoURL} className='logo-airline' alt=''></img>
+            <div className="card-title">
+                {data.name}
+                <div className="details">
+                    <div className="alliance">
+                        {alliance !== 'none' && alliance}
+                    </div>
+                    <div className="phone">
+                        {data.phone}
+                    </div>
+                    <div className="site">
+                        {data.site}
                     </div>
                 </div>
+            </div>
         </div>
     );
 }
